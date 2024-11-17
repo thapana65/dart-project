c393214ccfefe1815bb09cd6d94c77941ba92cf5
thapana65 <eitshien@gmail.com>
2024-11-18 03:44:45 +0700

    dart

--- input.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  stdout.writeln('Type something');
-  int? input = int.parse(stdin.readLineSync()!);
-  int re = input * 2;
-  stdout.writeln('You typed: $re');
-}
